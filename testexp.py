b6fd05ba9feaa522fdcb46eba9ec4e084c1ec9d0
James <jamesnogra@gmail.com>
2018-11-16 16:00:49 +0800

    Changes to the regular exp.

--- testexp.py
+++ testexp.py
@@ -1,5 +1,5 @@
 expression_num = '22/7'
 num, den = expression_num.split( '/' )
 print(num, ' and ', den)
-answer_number = str(round((int(num)/int(den)), 2))
+answer_number = str(round((float(num)/float(den)), 2))
 print('Answer is', answer_number)
